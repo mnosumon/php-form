5f0700704d2289ff2a0fc75e6ec6d77b9a61ec5a
mnosumon <mdnuruddin378350@gmail.com>
2025-01-05 12:32:26 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<h1 class="text-3xl font-bold underline text-white bg-black">
+    Hello world!
+  </h1>
+</body>
+</html>
